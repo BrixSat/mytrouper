941ac4387bb0ee3eefa241300b181071732dc2bc
Manabjyoti Sarma <manab_27@yahoo.com>
2017-06-04 15:56:55 +0530

    Try changing some text

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
+<?php // NOTE:
+Website: www.mytrouper.com
+Description: Online artist booking platform.
+ ?>
 <?php
-
 //error_reporting(1);
-
 define('SITE_FILE',dirname(__FILE__).'/');
 require_once SITE_FILE.'includes/inc.php';
-
 
 ?>
